8d6aacccb92b29b44caf2df6627e5b4894025894
Aditya vidap <aditya_vidap@persistent.com>
2026-07-13 20:12:03 +0530

    build fix

--- docker/create_docker_image.sh
+++ docker/create_docker_image.sh
@@ -11,6 +11,7 @@ fi
 WEBCLIENT_REPO=$(pwd)/..
 
 cd $WEBCLIENT_REPO
+pwd
 
 sed -i 's/services.i2b2.org/i2b2-core-server:8080/'   $WEBCLIENT_REPO/i2b2_config_domains.json
 # sed -i 's/debug: false/debug: true/'  $BASE/i2b2-webclient/i2b2_config_domains.json
@@ -18,7 +19,7 @@ sed -i 's/services.i2b2.org/i2b2-core-server:8080/'   $WEBCLIENT_REPO/i2b2_confi
 sed -i 's#127.0.0.1:8080/#i2b2-core-server:8080/#g'  $WEBCLIENT_REPO/proxy.php
 sed -i 's#http://services.i2b2.org#http://i2b2-core-server:8080#g' $WEBCLIENT_REPO/proxy.php
 
-docker build -t $docker_username/$docker_reponame:i2b2-webclient_$WEBCLIENT_TAG $WEBCLIENT_REPO/
+docker build -t $docker_username/$docker_reponame:i2b2-webclient_$WEBCLIENT_TAG $WEBCLIENT_REPO/docker/
 
 if [ "$CI" = "true" ]; then
     docker push $docker_username/$docker_reponame:i2b2-webclient_$WEBCLIENT_TAG
